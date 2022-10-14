a9c1feb36a146e4e4aeacff7ba61cf2382050723
durjoy sarkar <durjoywebdeveloper777@gmail.com>
2022-10-14 22:24:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.orsayqr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const pranCollection = client
+      .db("personal-inventory")
+      .collection("pran-products");
+
+    app.get("/pranGroup", async (req, res) => {
+      const result = await pranCollection.find().toArray()
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, () => {
+  console.log("server is running at", port);
+});
